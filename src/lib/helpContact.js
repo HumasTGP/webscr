404abc771ba3d6e8f514d722f00a3f044362cb84
HumasTGP <humastgp@gmail.com>
2026-08-12 08:58:37 +0700

    Centralize WhatsApp help contact

---
+++ src/lib/helpContact.js
@@ -0,0 +1,6 @@
+export const HELP_CONTACT = {
+  phone: "+62 858-1429-0877",
+  waNumber: "6285814290877",
+  waMessage: "Halo! Saya mengalami problem",
+  hours: "Senin-Jumat, 08.00-16.00 WIB",
+};
